b0615a9434d90bdb99089d7937dc9652346faa14
Praveen Kumar <68590996+Praveen701@users.noreply.github.com>
2024-01-29 10:03:49 +0530

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use \App\Models\User;
 
 class Post extends Model
 {
     use HasFactory;
+
+      public function user_detail()
+    {
+        return $this->hasOne(User::class,'id','user_id');
+    }
+    
 }
